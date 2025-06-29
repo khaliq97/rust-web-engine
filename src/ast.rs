5967600bdd2b830627025947abc10715eda9021a
Zaid Khaliq <osamakhaliq97@gmail.com>
2025-06-29 14:21:40 +0100

    Add assignment, function and object expressions

--- src/ast.rs
+++ src/ast.rs
@@ -1,6 +1,7 @@
 // This file contains the AST (Abstract Syntax Tree) structures that are shared
 // between the Parser and Interpreter.
 
+use std::rc::Rc;
 use crate::token::{Token, Literal};
 
 // https://tc39.es/ecma262/#prod-Statement
@@ -8,7 +9,30 @@ pub enum Statement {
     // TODO: Support a list of VariableDeclaration's as seen in the spec
     // Currently we only support one declaration on a single line
     VariableStatement(Box<VariableDeclarationStatement>),
-    ExpressionStatement(Box<ExpressionStatement>)
+    ExpressionStatement(Box<ExpressionStatement>),
+    BlockStatement(Box<BlockStatement>)
+}
+
+#[derive(Debug)]
+// https://tc39.es/ecma262/#prod-PropertyDefinition
+pub struct PropertyDefinition {
+    pub(crate) property_name: PropertyName,
+    pub(crate) assignment_expression: AssignmentExpression,
+}
+
+
+#[derive(Debug)]
+//https://tc39.es/ecma262/#prod-PropertyName
+// TODO: Support computed property names: https://tc39.es/ecma262/#prod-ComputedPropertyName
+pub enum PropertyName {
+    IdentifierName(Token),
+    LiteralPropertyName(Literal),
+}
+
+#[derive(Debug)]
+// https://tc39.es/ecma262/#prod-ObjectLiteral
+pub struct ObjectLiteralExpression {
+    pub property_definitions: Vec<PropertyDefinition>,
 }
 
 // https://tc39.es/ecma262/#prod-VariableStatement
@@ -20,34 +44,96 @@ pub struct VariableStatement {
 pub struct VariableDeclarationStatement {
     pub binding_identifier: Token,
     //TODO: The initializer should be of type AssignmentExpression(https://tc39.es/ecma262/#prod-AssignmentExpression)
-    pub initializer: Option<Box<ExpressionStatement>>
+    pub initializer: Option<Box<AssignmentExpression>>
 }
 
+#[derive(Debug)]
 // https://tc39.es/ecma262/#prod-AssignmentExpression
-pub enum AssignmentExpression {
-    LeftHandSideExpression(Box<LeftHandSideExpression>),
-}
-
-// https://tc39.es/ecma262/#prod-LeftHandSideExpression
-pub struct LeftHandSideExpression {
+pub struct AssignmentExpression {
+    // https://tc39.es/ecma262/#prod-LeftHandSideExpression
     // NewExpression TODO
     //  -> MemberExpression TODO
     //      -> PrimaryExpression (TODO: We're representing this as a ExpressionStatement for now, spec is confusing me)
-    pub expression: Box<ExpressionStatement>,
+    // At some point we'll split the LeftHandSideExpression out to it's own struct but this is ok for now
+    pub expression: Rc<ExpressionStatement>,
+    pub left_hand_side_expression: Rc<ExpressionStatement>
 }
 
+// https://tc39.es/ecma262/#prod-FunctionBody
+pub struct FunctionBody {
+    // https://tc39.es/ecma262/#prod-FunctionStatementList
+    // -> https://tc39.es/ecma262/#prod-StatementList
+    //  -> https://tc39.es/ecma262/#prod-StatementListItem
+    //   -> https://tc39.es/ecma262/#prod-Statement
+    statements: Vec<Statement>,
+
+}
+
+// https://tc39.es/ecma262/#prod-FormalParameter
+pub struct FormalParameter {
+    // https://tc39.es/ecma262/#prod-BindingElement
+    // -> https://tc39.es/ecma262/#prod-SingleNameBinding
+    //  -> https://tc39.es/ecma262/#prod-BindingIdentifier
+    binding_identifier: Token,
+
+}
+// https://tc39.es/ecma262/#prod-FormalParameters
+pub struct FormalParameters {
+    parameters: Vec<FormalParameter>,
+}
+
+//https://tc39.es/ecma262/#prod-FunctionDeclaration
+pub struct FunctionDeclaration {
+    pub binding_identifier: Token,
+    pub formal_parameters: FormalParameters,
+    pub function_body: FunctionBody,
+}
+
+#[derive(Debug)]
+// https://tc39.es/ecma262/#prod-CallExpression
+pub struct CallExpression {
+    pub(crate) callee: Box<ExpressionStatement>,
+    pub(crate) paren: Token,
+    pub(crate) arguments: Vec<ExpressionStatement>,
+}
+
+
+// https://tc39.es/ecma262/#prod-BlockStatement
+// BlockStatement[Yield, Await, Return] :
+//  Block[?Yield, ?Await, ?Return]
+pub struct BlockStatement {
+    //  Block[?Yield, ?Await, ?Return]
+    //  ->  { StatementList[?Yield, ?Await, ?Return]opt }
+    //    ->  StatementListItem[Yield, Await, Return] :
+    //          Statement[?Yield, ?Await, ?Return]
+    //          Declaration[?Yield, ?Await]
+    pub statements: Vec<Statement>,
+}
+
+pub trait Callable {
+    fn call() { 
+        
+    }
+}
+
+#[derive(Debug)]
 pub enum ExpressionStatement {
     BinaryExpression(Box<BinaryExpression>),
     LiteralExpression(Box<LiteralExpression>),
     ParenthesizedExpression(Box<ParenthesizedExpression>),
     UnaryExpression(Box<UnaryExpression>),
-    VariableExpression(Box<VariableExpression>),
+    IdentifierExpression(Box<IdentifierExpression>),
+    CallExpression(Box<CallExpression>),
+    ObjectLiteralExpression(Box<ObjectLiteralExpression>),
+    AssignmentExpression(Box<AssignmentExpression>)
 }
 
-pub struct VariableExpression {
+#[derive(Debug)]
+pub struct IdentifierExpression {
     pub binding_identifier: Token,
 }
 
+#[derive(Debug)]
 pub struct BinaryExpression {
     pub left: Box<ExpressionStatement>,
     pub right: Box<ExpressionStatement>,
@@ -59,10 +145,12 @@ pub struct LiteralExpression {
     pub value: Literal,
 }
 
+#[derive(Debug)]
 pub struct ParenthesizedExpression {
     pub expression: Box<ExpressionStatement>,
 }
 
+#[derive(Debug)]
 pub struct UnaryExpression {
     pub operator: Token,
     pub right: Box<ExpressionStatement>,
@@ -78,15 +166,20 @@ pub trait AstVisitor<R> {
     fn visit_literal(&mut self, expression: &LiteralExpression) -> R;
     fn visit_parenthesized(&mut self, expression: &ParenthesizedExpression) -> R;
     fn visit_unary(&mut self, expression: &UnaryExpression) -> R;
-    fn visit_variable_statement(&mut self, expression: &VariableDeclarationStatement) -> R;
-    fn visit_variable_expression(&mut self, expression: &VariableExpression) -> R;
+    fn visit_variable_declaration(&mut self, expression: &VariableDeclarationStatement) -> R;
+    fn visit_identifier_expression(&mut self, expression: &IdentifierExpression) -> R;
+    fn visit_call_expression(&mut self, expression: &CallExpression) -> R;
+    fn visit_block_statement(&mut self, expression: &BlockStatement) -> R;
+    fn visit_object_literal_expression(&mut self, expression: &ObjectLiteralExpression) -> R;
+    fn visit_assignment_expression(&mut self, expression: &AssignmentExpression) -> R;
 }
 
 impl<R> Accept<R> for Statement {
     fn accept<V: AstVisitor<R>>(&self, visitor: &mut V) -> R {
         match self {
             Statement::ExpressionStatement(e) => { visitor.visit_expression_statement(e) }
-            Statement::VariableStatement(v) => { visitor.visit_variable_statement(v) }
+            Statement::VariableStatement(v) => { visitor.visit_variable_declaration(v) }
+            Statement::BlockStatement(b) => { visitor.visit_block_statement(b) }
         }
     }
 }
@@ -98,7 +191,10 @@ impl<R> Accept<R> for ExpressionStatement {
             ExpressionStatement::LiteralExpression(l) => visitor.visit_literal(l),
             ExpressionStatement::ParenthesizedExpression(p) => visitor.visit_parenthesized(p),
             ExpressionStatement::UnaryExpression(u) => visitor.visit_unary(u),
-            ExpressionStatement::VariableExpression(v) => visitor.visit_variable_expression(v),
+            ExpressionStatement::IdentifierExpression(v) => visitor.visit_identifier_expression(v),
+            ExpressionStatement::CallExpression(c) => visitor.visit_call_expression(c),
+            ExpressionStatement::ObjectLiteralExpression(o) => visitor.visit_object_literal_expression(o),
+            ExpressionStatement::AssignmentExpression(a) => visitor.visit_assignment_expression(a),
             _=> unimplemented!()
         }
     }
--- src/ast_printer.rs
+++ src/ast_printer.rs
@@ -1,16 +1,28 @@
 // This file contains the ASTPrettyPrinter implementation that was extracted from interpreter.rs
 
-use crate::ast::{
-    AstVisitor, ExpressionStatement, BinaryExpression, LiteralExpression,
-    ParenthesizedExpression, UnaryExpression, VariableExpression, VariableDeclarationStatement,
-    Accept
-};
+use crate::ast::{AstVisitor, ExpressionStatement, BinaryExpression, LiteralExpression, ParenthesizedExpression, UnaryExpression, IdentifierExpression, VariableDeclarationStatement, Accept, CallExpression, BlockStatement, Statement, ObjectLiteralExpression, AssignmentExpression};
 use crate::token::Literal;
 
 pub struct ASTPrettyPrinter;
 
 impl ASTPrettyPrinter {
     fn parenthesize(&mut self, name: String, exprs: Vec<&ExpressionStatement>) -> String {
+        let mut builder = String::new();
+
+        builder.push('(');
+        builder.push_str(&name);
+
+        for expr in exprs {
+            builder.push(' ');
+            builder.push_str(&expr.accept(self));
+        }
+
+        builder.push(')');
+
+        builder
+    }
+
+    fn parenthesize_statement(&mut self, name: String, exprs: &[Statement]) -> String {
         let mut builder = String::new();
 
         builder.push('(');
@@ -68,21 +80,46 @@ impl AstVisitor<String> for ASTPrettyPrinter {
                     vec![&*node.right]
                 )
             },
-            ExpressionStatement::VariableExpression(node) => {
+            ExpressionStatement::IdentifierExpression(node) => {
                 return self.parenthesize(
-                    format!("VariableExpression {:?}", node.binding_identifier.literal),
+                    format!("VariableExpression {:?}", node.binding_identifier.lexeme),
                     vec![]
                 )
             }
             ExpressionStatement::ParenthesizedExpression(_) => {
                 return self.visit_parenthesized(node);
+            },
+            ExpressionStatement::CallExpression(node) => {
+                let mut args_to_string: String = String::new();
+                args_to_string.push_str("(");
+                for arg in &node.arguments {
+                    args_to_string.push_str(arg.accept(self).as_str());
+                    args_to_string.push_str(", ");
+                }
+                args_to_string.push_str(")");
+                return self.parenthesize(
+                    format!("CallExpression args: {:?}", args_to_string),
+                    vec![&*node.callee]
+                )
+            },
+            ExpressionStatement::ObjectLiteralExpression(node) => {
+                return self.parenthesize(
+                    format!("ObjectLiteralExpression"),
+                    vec![]
+                )
+            },
+            ExpressionStatement::AssignmentExpression(node) => {
+                return self.parenthesize(
+                    format!("AssignmentExpression"),
+                    vec![&*node.left_hand_side_expression, &*node.expression]
+                )
             }
         }
     }
 
-    fn visit_variable_expression(&mut self, expression: &VariableExpression) -> String {
+    fn visit_identifier_expression(&mut self, expression: &IdentifierExpression) -> String {
         return self.parenthesize(
-            format!("VariableExpression {:?}", expression.binding_identifier.literal),
+            format!("IdentifierExpression {:?}", expression.binding_identifier.lexeme),
             vec![]
         )
     }
@@ -94,12 +131,21 @@ impl AstVisitor<String> for ASTPrettyPrinter {
         )
     }
 
-    fn visit_variable_statement(&mut self, expression: &VariableDeclarationStatement) -> String {
+    fn visit_assignment_expression(&mut self, expression: &AssignmentExpression) -> String {
+        return self.parenthesize(
+            format!("AssignmentExpression"),
+            vec![&*expression.left_hand_side_expression, &*expression.expression]
+        )
+    }
+
+    fn visit_variable_declaration(&mut self, expression: &VariableDeclarationStatement) -> String {
         match &expression.initializer {
             Some(ref init_node) => {
+                let assignment_expression_string = self.visit_assignment_expression(init_node);
                 return self.parenthesize(
-                    format!("VariableDeclarationStatement {:?}", expression.binding_identifier.lexeme),
-                    vec![&**init_node]
+                    format!("[VariableDeclarationStatement] BindingIdentifier:{:?}, Initializer:{:?}", expression.binding_identifier.lexeme,
+                            assignment_expression_string),
+                    vec![]
                 )
             },
             None => {
@@ -109,5 +155,37 @@ impl AstVisitor<String> for ASTPrettyPrinter {
                 )
             }
         }
+    }
+
+    fn visit_object_literal_expression(&mut self, expression: &ObjectLiteralExpression) -> String {
+        let mut assignment_expressions: Vec<String> = Vec::new();
+        for property_definition in &expression.property_definitions {
+            assignment_expressions.push(self.visit_assignment_expression(&property_definition.assignment_expression));
+        }
+        return self.parenthesize(
+            format!("ObjectLiteralExpression PropertyDefinitions {}", assignment_expressions.join(", ")),
+            vec![]
+        )
+    }
+
+    fn visit_call_expression(&mut self, expression: &CallExpression) -> String {
+        let mut args_to_string: String = String::new();
+        args_to_string.push_str("(");
+        for arg in &expression.arguments {
+            args_to_string.push_str(arg.accept(self).as_str());
+            args_to_string.push_str(", ");
+        }
+        args_to_string.push_str(")");
+        return self.parenthesize(
+            format!("CallExpression args: {:?}", args_to_string),
+            vec![&*expression.callee]
+        )
+    }
+
+    fn visit_block_statement(&mut self, expression: &BlockStatement) -> String {
+        self.parenthesize_statement(
+            format!("BlockStatement"),
+            &*expression.statements
+        )
     }
 }
